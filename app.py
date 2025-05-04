e507c498529b1c60b7aeae68478003bc334a3e88
RaY-MbS60001 <sfisomabaso12242001@gmail.com>
2025-05-04 06:05:18 +0200

    app.run()

--- app.py
+++ app.py
@@ -872,5 +872,5 @@ if __name__ == "__main__":
     if os.environ.get('FLASK_ENV') == 'production':
         app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
     else:
-        app.run(debug=True)
+        app.run()
 
